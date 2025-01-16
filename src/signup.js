57b22015beacfc11a34592046f97d20a8ccbc0b3
kidlocchikapa <bed-com-48-22@unima.ac.mw>
2025-01-16 17:39:14 +0200

    check update02

--- src/signup.js
+++ src/signup.js
@@ -74,7 +74,7 @@ const Signup = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch('https://lockievisualbackend.onrender.com/signup', {
+      const response = await fetch('https://lockievisualbackend.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
